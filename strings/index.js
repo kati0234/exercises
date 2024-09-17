492f690881688103048bfd398fc4ab5ee521de6b
kati0234 <katinkaasp@gmail.com>
2024-09-17 13:23:22 +0200

    øvelse

---
+++ strings/index.js
@@ -0,0 +1,47 @@
+// console.log("math.random", Math.floor(Math.random() * 3));
+
+// let x = math.floor(math.random() * 3) + 1;
+// console.log(x);
+// const n1 = 1;
+// const n2 = 2;
+
+// const s1 = "1";
+// const s2 = "2";
+
+// console.log("n1+n2", n1 + n2);
+// console.log("s1+s2", s1 + s2);
+// console.log("n1+s2", n1 + s2);
+// console.log("s1+n2", s1 + n2);
+
+console.log('""+1+0=', "" + 1 + 0);
+// 10
+console.log('""-1+0=', "" - 1 + 0);
+// -1
+console.log("true + false", true + false);
+// 1
+console.log('6 / "3"', 6 / "3");
+// 2
+console.log("“12“-0=", "12" - 0);
+// 12
+console.log('+"12"=', +"12");
+// 12
+console.log('"$" + 4 + 5', "$" + 4 + 5);
+// $45
+console.log('4 + 5 + "px"', 4 + 5 + "px");
+// 9px
+console.log('"4" - 2', "4" - 2);
+// 2
+console.log("7 / 0", 7 / 0);
+// infinti
+console.log('"2"*"3"', "2" * "3");
+// 6
+console.log('"4px" - 2', "4px" - 2);
+// NaN
+console.log('"-9"+5', "-9" + 5);
+// -9 5
+console.log('"-9"-5', "-9" - 5);
+// -14
+console.log("null+1", null + 1);
+// 1
+console.log("undefined + 1 ", undefined + 1);
+// NaN
